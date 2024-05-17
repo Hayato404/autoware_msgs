07dfbfc6ba328574cec0d3c4b52df9a58e93e2fa
Hayato404 <md23083@shibaura-it.ac.jp>
2024-05-17 17:50:46 +0900

    first commit

--- autoware_msgs_ros2/build/autoware_msgs/autoware_msgs__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hiroselab/HDT-ros1-bridge/custom_msgs/custom_msgs_ros2/build/autoware_msgs/autoware_msgs__py
-# Build directory: /home/hiroselab/HDT-ros1-bridge/custom_msgs/custom_msgs_ros2/build/autoware_msgs/autoware_msgs__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
